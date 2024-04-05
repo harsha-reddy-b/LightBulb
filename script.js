911a8feb0aa0c938ab706b9d2ba23c5387aed26e
harsha-reddy-b <156340070+harsha-reddy-b@users.noreply.github.com>
2024-04-05 12:31:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function light(){
     var lightbulb = document.getElementById("bulb")
     if (lightbulb.src.match("bulboff")){
-        lightbulb.src="img/bulbon.gif"
+        lightbulb.src="bulbon.gif"
     }
     else{
-        lightbulb.src="img/bulboff.gif"
+        lightbulb.src="bulboff.gif"
     }
 
 
